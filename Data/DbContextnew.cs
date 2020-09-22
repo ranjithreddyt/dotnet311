71057a80b4103a8079e251a97a11bed9dfe30d33
Ranjith Tadisina <ranjith.tadisina@va.gov>
2020-09-21 23:19:47 -0500

    first

---
+++ Data/DbContextnew.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SampleDBConn.Model;
+
+    public class DbContextnew : DbContext
+    {
+        public DbContextnew (DbContextOptions<DbContextnew> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SampleDBConn.Model.Employee> Employee { get; set; }
+
+        public DbSet<SampleDBConn.Model.Persons> Persons { get; set; }
+    }
---
+++ Model/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SampleDBConn.Model
+{
+    public  class Employee
+    {
+        [Key]
+        public int EmpID { get; set; }
+        public string EmpLastName { get; set; }
+        public string EmpFirstName { get; set; }
+        public string EmpOrganization { get; set; }
+        public string EmpCity { get; set; }
+    }
+}
+
